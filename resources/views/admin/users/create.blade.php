98081bbd932d18c49835f8270fc131719c3e951a
cabavas <cabavas@gmail.com>
2024-10-29 14:27:14 -0300

    Aula 14

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -1,15 +1,9 @@
-@extends('admin.layouts.app')
+@extends("admin.layouts.app")
 
-@section('content')
-<h1>Novo Usuário</h1>
+@section("content")
+    <h1>Novo Usuário</h1>
 
- <x-alert/>
-
-<form action="{{route('users.store')}}" method="POST">
-    @csrf()
-    <input type="text" name="name" placeholder="Nome" value="{{old('name')}}">
-    <input type="email" name="email" placeholder="E-mail" value="{{old('email')}}">
-    <input type="password" name="password" placeholder="Senha">
-    <button type="submit">Enviar</button>
-</form>
+    <form action="{{ route("users.store") }}" method="POST">
+        @include("admin.users.partials.form")
+    </form>
 @endsection
--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -1,16 +1,12 @@
-@extends('admin.layouts.app')
+@extends("admin.layouts.app")
 
-@section('title', 'Editar o Usuário')
+@section("title", "Editar o Usuário")
 
-@section('content')
-<h1>Editar o Usuário {{ $user->name }}</h1>
-<x-alert />
-<form action="{{ route('users.update', $user->id) }}" method="POST">
-    @csrf()
-    @method('put')
-    <input type="text" name="name" placeholder="Nome" value="{{$user->name}}">
-    <input type="email" name="email" placeholder="E-mail" value="{{$user->email}}">
-    <input type="password" name="password" placeholder="Senha">
-    <button type="submit">Enviar</button>
-</form>
+@section("content")
+    <h1>Editar o Usuário {{ $user->name }}</h1>
+    <x-alert />
+    <form action="{{ route("users.update", $user->id) }}" method="POST">
+        @method("put")
+        @include("admin.users.partials.form")
+    </form>
 @endsection
