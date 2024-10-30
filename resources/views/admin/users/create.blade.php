c8cda4d8616cb0d67761f6a305d2812c806e6df5
cabavas <cabavas@gmail.com>
2024-10-30 12:07:18 -0300

    Curso finalizado

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -1,7 +1,10 @@
 @extends("admin.layouts.app")
 
 @section("content")
-    <h1>Novo Usuário</h1>
+@include('admin.users.partials.breadcrumb')
+<div class="py-6">
+    <h2 class="font-semibold text-xl text-gray-800 dark:text-gray">Novo Usuário</h2>
+</div>
 
     <form action="{{ route("users.store") }}" method="POST">
         @include("admin.users.partials.form")
--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -4,7 +4,13 @@
 
 @section("content")
     <h1>Editar o Usuário {{ $user->name }}</h1>
-    <x-alert />
+    @include('admin.users.partials.breadcrumb')
+    <div class="py-6">
+        <div class="font-semibold text-xl text-gray-800 dar:text-gray">
+            Editar o Usuário {{ $user->name }}
+        </div>
+    </div>
+    
     <form action="{{ route("users.update", $user->id) }}" method="POST">
         @method("put")
         @include("admin.users.partials.form")
